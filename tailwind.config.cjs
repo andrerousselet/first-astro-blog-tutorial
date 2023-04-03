743f0ff6a3036966d6df603551eee9e26bf0aed9
Andre Rousselet <andre098@hotmail.com>
2023-04-03 14:54:38 -0300

    feat(custom styling): add fonts and custom colors to tailwind config
    apply them in the nav component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': 'hsl(235, 40%, 5%)',
+				'primary-hover': 'hsl(235, 40%, 5%, 0.9)',
+				'secondary': 'hsl(235, 55%, 50%)',
+				'secondary-hover': 'hsl(235, 55%, 50%, 0.9)',
+			},
+			fontFamily: {
+        'sans': ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans],
+      },
+		},
 	},
 	plugins: [],
 }
