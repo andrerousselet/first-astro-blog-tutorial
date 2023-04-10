7195b4a17f95127b995dd151f48b283aa070f514
Andre Rousselet <andre098@hotmail.com>
2023-04-10 11:04:53 -0300

    feat(utils): add function to filter(reduce) and sort posts to be shown in blog posts page

--- src/js/utils.js
+++ src/js/utils.js
@@ -10,7 +10,37 @@ export function slugify(text) {
 }
 
 export function formatDate(date) {
-  return new Date(date).toLocaleDateString('pt-BR', {
+  return new Date(date).toLocaleDateString("pt-BR", {
     timeZone: "UTC",
   });
+}
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc, curr) => {
+    const { date, draft } = curr.frontmatter;
+    if (filterOutDrafts && draft) return acc;
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+    acc.push(curr);
+    return acc;
+  }, [])
+
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  if (typeof limit === 'number') {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
 }
